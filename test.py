86019637830cb8eff4ea970a521a92a42a7f6a25
xKoc0 <bautitheone@gmail.com>
2023-06-13 17:44:44 -0300

    Interpret for strings on cmd, some branch setup

--- test.py
+++ test.py
@@ -1,28 +1,40 @@
-def variableTypeCheck():
-    varTesting = {2, "esta", 6.5, "prueba" : 30}
-    print("Variable: ", varTesting, ", Tipo: ", type(varTesting))
-    
-def variableConversion():
-    x = 2.5
-    y = 3
-    z = 8j    
-    print("Unconverted Numbers: ", "\n", "x: ", x ,type(x), "\n", "y: ", y , type(y), "\n", "z: ", z, type(z), "\n")    
-    
-    x = 2
-    y = 3.5
-    print("Converted Numbers: ", "\n", "x: ", x ,type(x), "\n", "y: ", y , type(y), "\n", "z: ", z, type(z), "Can't convert complex numbers", "\n")
-    
-    #a = int(x)
-    #b = float(y)
-    #print("Converted using other variables: ", "\n", a , type(a), "\n", b , type(b))
-    
-def stringTesting():
-    string_var = str(5.4)
-    print("Testing python string function with other variable types: " ,string_var)
+import sys
+#Basic functions meant for easy testing on cmd while checking the script   
 
+def interpret(val):
+    try:
+        return int(val)
+    except ValueError:
+        try:
+            return float(val)
+        except ValueError:
+            try:
+                return list(val)
+            except:
+                try:
+                    return tuple(val)
+                except:
+                    return val
+                           
+def varTypeCheck(x):
+    #x = input("Write a variable Float, Int, String, List or Touple: ")
+    a = interpret(x)
+    print("Variable: ", x, type(a))
+  
+def variableConversion(x,y,a,b):   
+    print("From: ", "\n", a , type(a), "\n", b, type(b))
+    a = int(x)
+    b = float(y)
+    print("To: ", "\n", a , type(a), "\n", b , type(b))
+    
 def stringArrayCheck(x):
     strArrayVar = "012345"
     print("The character on the array position is: " ,strArrayVar[x])
+ 
+#Allows calling any global function on the file, only for testing purposes!
+if __name__ == '__main__':
+    indice = sys.argv
+    globals()[indice[1]](*indice[2:]) 
     
-"testing"
-
+input("Press anything to close the window")
+#print(sys.version)
