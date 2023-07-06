4a329bd66b3fc88ed9eada2df99d9a5cfb06f00e
xKoc0 <bautitheone@gmail.com>
2023-07-06 15:06:48 -0300

    Cleaned up the comments

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-"""
 import sys
 import msvcrt as m
 
@@ -12,66 +11,6 @@ class BegginerExample:
         else:
             print("Variable:", x, type(a))
     
-    def interpret(self, val):
-        try:
-            return int(val)
-        except ValueError:
-            try:
-                return float(val)
-            except ValueError:
-                try:
-                    return list(val)
-                except ValueError:
-                    try:
-                        return tuple(val)
-                    except ValueError:
-                        return "error"
-                        
-    def variableConversion(self, x, y, a, b):
-        print("From:\n", a, type(a), "\n", b, type(b))
-        a = int(x)
-        b = float(y)
-        print("To:\n", a, type(a), "\n", b, type(b))
-        
-    def stringArrayCheck(self, x, arrayVar):
-        strArrayVar = arrayVar
-        print("The array is:", strArrayVar, "The character at the array position is:", strArrayVar[x])
-
-
-exampleObject = BegginerExample()
-
-def wait():
-    m.getch()
-    
-def startProgram():
-    chooseVar = input('Do you want to do a "Variable type check" (vtc), do a "Variable conversion" (vc) or do a "String array check"? (sac)  ')
-    if chooseVar == "vct":
-        BegginerExample.varTypeCheck()
-    elif chooseVar == "vc":
-        BegginerExample.variableConversion89
-    elif chooseVar == "sac":
-        BegginerExample.stringArrayCheck()
-    else:
-        print('Please input a valid answer and try again! (press any key to continue)')
-        wait()
-        startProgram()
-
-if __name__ == __name__:
-    startProgram()
-    
-input("Press anything to close the window")
-"""
-
-class BegginerExample:
-
-    def varTypeCheck(self):
-        x = input("Write a variable Float, Int, String, List, or Tuple: ")
-        a = self.interpret(x)
-        if a == "error":
-            print("Wrong variable format!")
-        else:
-            print("Variable:", x, type(a))
-   
     def interpret(self, val):
 
         if val.startswith("[") and val.endswith("]"):
@@ -101,20 +40,25 @@ class BegginerExample:
                     except ValueError:
                         return "error"
 
-    def variableConversion(self, x, y, a, b):
-        print("From:\n", a, type(a), "\n", b, type(b))
-        a = int(x)
-        b = float(y)
-        print("To:\n", a, type(a), "\n", b, type(b))
-        
-    def stringArrayCheck(self, x, arrayVar):
-        strArrayVar = arrayVar
-        print("The array is:", strArrayVar, "The character at the array position is:", strArrayVar[x])
-
 def wait():
     m.getch()
+"""    
+def startProgram():
+    chooseVar = input('Do you want to do a "Variable type check" (vtc), do a "Variable conversion" (vc) or do a "String array check"? (sac)  ')
+    if chooseVar == "vct":
+        BegginerExample.varTypeCheck()
+    elif chooseVar == "vc":
+        BegginerExample.variableConversion89
+    elif chooseVar == "sac":
+        BegginerExample.stringArrayCheck()
+    else:
+        print('Please input a valid answer and try again! (press any key to continue)')
+        wait()
+        startProgram()
 
-
+if __name__ == __name__:
+    startProgram()
+"""    
 exampleObject = BegginerExample()
 exampleObject.varTypeCheck()
 print("Press anything to close the window")
